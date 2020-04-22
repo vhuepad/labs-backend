128c2cf0cfc4990fb3b05e9d5dd5d2f3206416ca
sdmoreno <sdmorenob@unal.edu.co>
2020-04-22 03:05:03 -0500

    Spring boot project for UNal Software Engineering II

---
+++ src/main/java/co/edu/unal/software_engineering/labs/controller/RoleController.java
@@ -0,0 +1,25 @@
+package co.edu.unal.software_engineering.labs.controller;
+
+import co.edu.unal.software_engineering.labs.model.Role;
+import co.edu.unal.software_engineering.labs.service.RoleService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RoleController{
+
+    private RoleService roleService;
+
+
+    public RoleController( RoleService roleService ){
+        this.roleService = roleService;
+    }
+
+
+    @GetMapping( value = { "/roles" } )
+    public List<Role> getAllRoles( ){
+        return roleService.getAll( );
+    }
+}
